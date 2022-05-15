65d51988ab3290dfbc0009209febef87c32699f1
cassandra-mn <arthenciac@gmail.com>
2022-05-15 14:28:42 -0300

    add cart

--- src/componentes/CartPage.js
+++ src/componentes/CartPage.js
@@ -1,5 +1,93 @@
+import {TailSpin} from 'react-loader-spinner';
+import {useNavigate} from 'react-router-dom';
+import {useState, useEffect} from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+
 export default function CheckoutPage() {
-    return (
-        <>Essa é a tela do carrinho</>
-    );
+    const [cart, setCart] = useState();
+    const navigate = useNavigate();
+    
+    useEffect(() => {
+        async function getProduct() {
+            const products = await axios.get(`http://localhost:5000/cart`);
+            setCart(products.data);
+        }
+        getProduct();
+    }, []);
+
+    console.log(cart);
+    if (cart) console.log(cart.length);
+
+    return cart ? (
+        cart.length > 0 ? (
+            <Container>
+            {cart.map(product => {
+                return (
+                    <p>
+                    <Image src={product.image} />
+                    <Name>{product.name}</Name>
+                    <Price>R${product.price}</Price>
+                    <Cont>{product.amount}</Cont>
+                    <Del onClick={() => alert('delete')}>Deletar</Del>
+                    </p>
+                );
+            })} 
+            </Container>
+        ) : (
+            <Container>
+                <Text>Seu carrinho está vazio</Text>
+                <Back onClick={() => navigate('/')}>Adicionar produtos</Back>
+            </Container>
+        )) : 
+        <Loading><TailSpin color='#D2691E'/></Loading>;
 }
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    overflow-x: hidden;
+    position: relative;
+`;
+
+const Image = styled.img`
+
+`;
+
+const Name = styled.h1`
+
+`;
+
+const Price = styled.p`
+
+`;
+
+const Cont = styled.div`
+
+`;
+
+const Del = styled.button`
+
+    :hover {
+        cursor: pointer;
+    }
+`;
+
+const Text = styled.p`
+
+`;
+
+const Back = styled.button`
+
+    :hover {
+        cursor: pointer;
+    }
+`;
+
+const Loading = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
--- src/componentes/ProductPage.js
+++ src/componentes/ProductPage.js
@@ -8,8 +8,8 @@ export default function ProductPage() {
     const {id} = useParams();
     const navigate = useNavigate();
     const [product, setProduct] = useState();
-    const [amount, setAmount] = useState(0);
-    
+    const [amount, setAmount] = useState(1);
+  
     useEffect(() => {
         async function getProduct() {
             const product = await axios.get(`http://localhost:5000/product/${id}`);
@@ -17,6 +17,26 @@ export default function ProductPage() {
         }
         getProduct();
     }, []);
+
+    async function cart() {        
+        try {
+            const body = {...product, price: parseFloat(product.price.replace(',', '.')), amount};
+            await axios.post('http://localhost:5000/cart', body);
+            alert('Produto adicionado ao carrinho');
+        } catch(e) {
+            if (e.response.status === 403) alert('O produto já foi adicionado ao carrinho');
+        }
+    }
+
+    async function checkout() {
+        try {
+            const body = {...product, price: parseFloat(product.price.replace(',', '.')), amount};
+            await axios.post('http://localhost:5000/cart', body);
+            navigate('/cart');
+        } catch(e) {
+            if (e.response.status === 403) navigate('/cart');
+        }
+    }
 
     return product ? (
         <Container>
@@ -28,8 +48,9 @@ export default function ProductPage() {
                 {amount}
                 <button className='more' onClick={() => setAmount(amount + 1)}>+</button>
             </Cont>
-            <Cart onClick={() => navigate('/cart')}>Adicionar ao carrinho</Cart>
-            <Buy onClick={() => navigate('/checkout')}>Comprar agora</Buy>
+            <Cart onClick={cart}>Adicionar ao carrinho</Cart>
+            <Buy onClick={checkout}>Comprar agora</Buy>
+            <Text>{product.description}</Text>
         </Container>
     ) : <Loading><TailSpin color='#D2691E'/></Loading>;
 }
@@ -75,6 +96,10 @@ const Buy = styled.button`
     :hover {
         cursor: pointer;
     }
+`;
+
+const Text = styled.p`
+
 `;
 
 const Loading = styled.div`
